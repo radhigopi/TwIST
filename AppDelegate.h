db7c2cb4d90b0839d624ac61ffae6c713e168ea7
radhigopi <radhi_it@hotmail.com>
2017-05-30 16:26:19 -0500

    Initial commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorldTest
+//
+//  Created by Orland Hoeber on 2015-01-12.
+//  Copyright (c) 2015 Radhika Gopi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
